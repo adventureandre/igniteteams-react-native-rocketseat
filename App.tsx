003e7be05baea34025b7fbb1cdd60ec2f9b7ab97
Andre Luiz <adventureandre@hotmail.com>
2024-02-12 16:11:23 -0300

    Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {useFonts, Roboto_400Regular, Roboto_700Bold}from'@expo-google-fonts/robo
 import Groups from '@screens/Groups';
 import theme from '@theme/index';
 import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -11,6 +12,11 @@ export default function App() {
 
   return (
   <ThemeProvider theme={theme}>
+    <StatusBar
+    barStyle="light-content"
+    backgroundColor="transparent"
+    translucent
+    />
 {fontsLoader ? <Groups/> : <Loading/>}
   </ThemeProvider>
    
