e86bdeb8040dd1393711bccc4dc3e108a6e5ebdd
Andre Luiz <adventureandre@hotmail.com>
2024-02-12 16:07:08 -0300

    Componente de Loading

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
   <ThemeProvider theme={theme}>
-{!fontsLoader ? <Groups/> : <Loading/>}
+{fontsLoader ? <Groups/> : <Loading/>}
   </ThemeProvider>
    
    )
