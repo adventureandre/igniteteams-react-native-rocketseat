febfdd0b77fb78a3c207867521d958a8550c1adb
Andre Luiz <adventureandre@hotmail.com>
2024-02-12 19:54:11 -0300

    Criando Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 
 export default function Groups() {
   return (
     <Container>
       <Header/>
+      <Highlight
+      title='Turmas'
+      subtitle='Jogue com sua turma'
+      />
     </Container>
   );
 };
