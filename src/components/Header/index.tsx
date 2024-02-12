767ee4000d53ac5b66df32c47de9b737d83e05ee
Andre Luiz <adventureandre@hotmail.com>
2024-02-12 18:44:04 -0300

    Biblioteca de ícones

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
-import { Container, Logo } from "./styles";
+
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from "@assets/logo.png";
 
-export function Header(){
+
+type Props = {
+    showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: Props) {
 
     return (
         <Container>
-            <Logo source={logoImg}/>
+            {
+                showBackButton &&
+                <BackButton><BackIcon /></BackButton>
+            }
+            <Logo source={logoImg} />
         </Container>
     )
 }
