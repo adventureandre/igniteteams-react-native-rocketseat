defd874fe5441bbc015f7e777d4832b7f7f1c3fc
Andre Luiz <adventureandre@hotmail.com>
2024-02-23 23:44:48 -0300

    Criando Componente Input Fixes

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
     return (
@@ -10,11 +11,13 @@ export function NewGroup() {
             <Content>
                 <Icon />
                 <Highlight
-                title="Nova Turma"
-                subtitle="Crie a turma para adicionar as pessoas"
+                    title="Nova Turma"
+                    subtitle="Crie a turma para adicionar as pessoas"
                 />
+                <Input placeholder="Nome da turma" />
                 <Button
-                title="Criar"
+                    title="Criar"
+                    style={{ marginTop: 20 }}
                 />
             </Content>
         </Container>
