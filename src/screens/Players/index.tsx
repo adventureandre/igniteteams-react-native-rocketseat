bd4de42f5421e873b31800a5751a21405c35f802
Andre Luiz <adventureandre@hotmail.com>
2024-02-27 20:59:14 -0300

    Criando componente Button Icon Fixes #30

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/Buttonicon";
 
 export function Players() {
     return (
@@ -8,6 +9,7 @@ export function Players() {
             <Header showBackButton />
             <Highlight title="Nome da Turma" 
             subtitle="adicione a galera e separe os times" />
+            <ButtonIcon icon="rocket" />
         </Container>
     )
 }
