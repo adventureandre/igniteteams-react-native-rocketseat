46b5e86b171093051ab1464f867978642d0ea860
Andre Luiz <adventureandre@hotmail.com>
2024-02-27 23:14:16 -0300

    Create of Filter Component Fixes #32

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -6,3 +6,13 @@ background-color: ${({theme})=> theme.COLORS.GRAY_600} ;
 
 padding: 24px;
 `
+
+export const Form = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  
+  flex-direction: row;
+  justify-content: center;
+
+  border-radius: 6px;
+`;
